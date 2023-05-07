ceb7d10cbff8a0a8f95ebf1bfa2c14dafc29f8fe
DarkLord76865 <mmima191@gmail.com>
2023-05-07 20:31:23 +0200

    work on threadpool
    finished threadpool implementation from Book tutorial

--- src/parallelism.rs
+++ src/parallelism.rs
@@ -1,24 +1,10 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-}
-
-impl ThreadPool {
-    // --snip--
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-
-        let mut workers = Vec::with_capacity(size);
-
-        for id in 0..size {
-            workers.push(Worker::new(id));
-        }
-
-        ThreadPool { workers }
-    }
-    // --snip--
+    sender: mpsc::Sender<Job>,
 }
 
 struct Worker {
@@ -26,12 +12,56 @@ struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
-impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread_builder = thread::Builder::new();
-        let thread = thread_builder.spawn(|| {}).unwrap();
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
-        Worker { id, thread }
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Self { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread_builder = thread::Builder::new();
+        let thread = thread_builder.spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        }).unwrap();
+        // this is done this way to catch the error when the thread can't spawn, right now it isn't implemented so that's why there is unwrap
+
+        Self { id, thread }
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        assert!(true);
+    }
+}
